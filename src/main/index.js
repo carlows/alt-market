c0377fea997f9b0b0a1c0ac6552b85e7f7a520ab
Alexis Duran <alexis@hashlabs.com>
2017-08-11 19:18:49 -0400

    Remove deprecated nested folder

--- src/main/index.js
+++ src/main/index.js
@@ -3,8 +3,8 @@
 import React, { Component } from 'react';
 import { Switch } from 'react-router';
 import { PrivateRoute, GuestRoute} from './routes';
-import Login from '../session/views/Login';
-import Feed from '../feed/views/Feed';
+import Login from '../session/views/login.js';
+import Feed from '../feed/views/feed.js';
 
 class App extends Component {
   render() {
