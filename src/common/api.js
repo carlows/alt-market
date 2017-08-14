df61d569891a73971196c2319b36dede43540e1a
Alexis Duran <alexis@hashlabs.com>
2017-08-14 13:07:31 -0400

    Static fixes for api.js file on common folder

--- src/common/api.js
+++ src/common/api.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
 const endpoint = 'http://localhost:4000';
-const buildUrl = (url) => (`${endpoint}${url}`);
+const buildUrl = url => `${endpoint}${url}`;
 
 /*
  * Defines all the API endpoints outside of the GraphQL API
  */
 const apiEndpoints = {
-  loginUser: ({ email, password }) => axios.post(buildUrl('/login'), {
-    auth: {
-      email,
-      password
-    }
-  })
+  loginUser: ({ email, password }) =>
+    axios.post(buildUrl('/login'), {
+      auth: {
+        email,
+        password
+      }
+    })
 };
 
 export default apiEndpoints;
